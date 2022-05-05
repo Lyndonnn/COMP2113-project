5818578cfae737e72f0bc1da79dbba6c4b284903
Lyndonnn <101377093+Lyndonnn@users.noreply.github.com>
2022-05-05 23:29:48 +0800

    Add files via upload

---
+++ data_output.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <fstream>
+#include <iomanip>
+#include "data_output.h"
+//#include "covid.h"
+using namespace std;
+
+struct Day {
+	int fund;
+	int support;
+	int infection;
+	double death;
+	int potential;
+	bool flag;
+	Day *next;
+};
+
+// output daily information to file
+void dataoutput() {
+	Day *head = NULL;
+
+	ofstream fout;
+	fout.open("dailyInfo.txt");
+
+	if (fout.fail()) {
+		cout << "Error in file opening!" << endl;
+		exit(1);
+	}
+
+	int count = 0;
+	Day *traverse = head;
+	// print column names
+	fout << "Day" << "   " << "Funding" << "   " << "Support Rate"
+	     << "   " << "Infection Rate" << "   " << "Death Rate" << endl;
+	// print daily information sequentially
+	while (traverse->next != NULL) {
+		fout << setw(3) << count << "   " << setw(8) << traverse->fund << "   "
+		     << setw(15) << traverse->support << "   " << setw(17) <<
+		     traverse->infection << "   " << setw(19) << traverse->death << endl;
+		count++;
+		traverse = traverse->next;
+	}
+
+	fout << setw(3) << count << "   " << setw(8) << traverse->fund << "   "
+	     << setw(15) << traverse->support << "   " << setw(17) <<
+	     traverse->infection << "   " << setw(19) << traverse->death << endl;
+	fout.close();
+}
