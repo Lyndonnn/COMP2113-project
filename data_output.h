040bdec2ebae7bff85a772b8e1cb88567714a4a1
Lyndonnn <101377093+Lyndonnn@users.noreply.github.com>
2022-05-07 01:08:54 +0800

    Update data_output.h

--- data_output.h
+++ data_output.h
@@ -1,5 +1,14 @@
 #ifndef data_output_H
 #define data_output_H
+struct Day {
+	int fund;
+	int support;
+	int infection;
+	double death;
+	int potential;
+	bool flag;
+	Day *next;
+};
 
 void dataoutput(*&head);
 
